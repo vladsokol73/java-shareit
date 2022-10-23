04c9d1abc753650d23e9b5210ac4ce2c99aa3122
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2022-10-23 22:54:15 +0400

    Add files via upload

--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoDate.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoDate.java
@@ -1,11 +1,15 @@
 package ru.practicum.shareit.item.dto;
 
 import lombok.Data;
+import ru.practicum.shareit.booking.Booking;
 
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
-public class ItemDto {
+public class ItemDtoDate {
+
     private Integer id;
 
     @NotBlank(message = "name не может быть пустым")
@@ -22,4 +26,9 @@ public class ItemDto {
 
     private Integer request;
 
+    private Booking lastBooking;
+
+    private Booking nextBooking;
+
+    private Set<CommentDto> comments = new HashSet<>();
 }
