fb744a4244b61dbca8b6cfeee90dd391fb1c8049
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2022-12-06 20:49:26 +0400

    Delete src directory

--- src/main/java/ru/practicum/shareit/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/ValidateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit;
-
-public class ValidateException extends RuntimeException {
-    public ValidateException(String message) {
-        super(message);
-    }
-}
