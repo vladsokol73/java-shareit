c781a7c1951502db31b34b5abda44e87cef68c0b
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2022-12-07 22:08:21 +0400

    Add files via upload

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
     public boolean existsByEmail(String email);
 }
--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.constraints.Email;
 import java.util.Collection;
 
 @Service
