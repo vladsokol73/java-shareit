8fa8a0498fea12703300c759bc12744a1279e005
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2022-12-14 21:34:14 +0400

    Delete src directory

--- src/main/java/ru/practicum/shareit/exceptions/ResponseValidateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class ResponseValidateException extends RuntimeException {
-    public ResponseValidateException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exceptions/UnsupportedStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class UnsupportedStateException extends RuntimeException {
-    public UnsupportedStateException(String message) {
-        super(message);
-    }
-}
