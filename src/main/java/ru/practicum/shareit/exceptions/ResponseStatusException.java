d1be685e38fd3cadf3dbb84370b11b1a331632b0
vladsokol73 <83404922+vladsokol73@users.noreply.github.com>
2022-12-14 21:31:40 +0400

    Delete src directory

--- src/main/java/ru/practicum/shareit/exceptions/ResponseStatusException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class ResponseStatusException extends RuntimeException {
-    public ResponseStatusException(String message) {
-        super(message);
-    }
-}
